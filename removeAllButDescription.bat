054b3b25a7110ba22b441d7f618774a4e8b40b28
Paulius Sereiva <paulius.sereiva@mif.stud.vu.lt>
2017-01-06 21:14:05 +0200

    Initial commit

---
+++ removeAllButDescription.bat
@@ -0,0 +1,3 @@
+"exiftool(-k).exe" -overwrite_original ^
+-all= -tagsFromFile @ -exif:ImageDescription -iptc:Caption-Abstract -iptc:CodedCharacterSet ^
+%*
---
+++ removeAllButDescriptionGPSSphere.bat
@@ -0,0 +1,3 @@
+"exiftool(-k).exe" -overwrite_original ^
+-all= -tagsFromFile @ -gps:all -exif:ImageDescription -iptc:Caption-Abstract -iptc:CodedCharacterSet -ProjectionType ^
+%*
